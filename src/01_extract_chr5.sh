7bf759340346528a58cfb3ddc8957536ef0b6693
Amy Olex <alolex@vcu.edu>
2019-10-31 10:27:18 -0400

    Updated extraction script.
    Added command line arguments, and added the deletion of temporary files.

--- src/01_extract_chr5.sh
+++ src/01_extract_chr5.sh
@@ -1,27 +1,106 @@
 #!/bin/bash
 
+# Copyright (c) 2018
+# Amy L. Olex, Virginia Commonwealth University
+# alolex at vcu.edu
+
 # Script that extracts chromosome 5 from each of the input files and writes the reads to a new BAM file.
 
-cat $1 | while read bam
+## Usage Function
+usage()
+{
+cat << EOF
+usage: $0 options
+This script is used to extract chromosome 5 from each of the input files and writes the reads to a new BAM file. Only read pairs with at least one mate mapped to chr5 are selected. The BAM file is then converted back into a paired FastQ file for re-alignment to the transcriptome and read counting done by Salmon.
+
+REQUIRED ARGUMENTS:
+        -f	Input file with list of BAM files to process.  *Must include full path to files.*
+
+OPTIONAL ARGUMENTS:
+        -d              Directory of where results should be saved.  Sub-folder 01_chr5 will be created here.
+
+-f INPUT File:
+Input file with list of BAM files to process.  *Must include full path to files.*
+
+-d RESULTS Directory:
+Directory where results folder should be created.
+
+EXAMPLE USAGE:
+   >> ~/01_extract_chr5.sh -f /path/to/filelist.txt -d /path/to/results/dir/
+
+EOF
+}
+
+
+## Parsing input arguments
+
+FILE=
+DIR=
+while getopts “hf:d:” OPTION
 do
-	
-	#samtools index ../$bam
-	# Get all mapped reads where both mates are mapped
-	samtools view -bh -f 1 -F 12 ../$bam chr5 > $bam.paired.chr5.bam
-	
-	# Get reads where R1 is unmapped, R2 is mapped in a primary alignment
-	samtools view -bh -f 4 -F 264 ../$bam chr5 > $bam.matemapped.chr5.bam
- 
-	# Get reads where R1 is mapped, and R2 is unmapped
-	samtools view -bh -f 8 -F 260 ../$bam chr5 > $bam.mateunmapped.chr5.bam
-
-	# merge bam files
-	samtools merge $bam.merged.chr5.bam  $bam.paired.chr5.bam $bam.matemapped.chr5.bam $bam.mateunmapped.chr5.bam
-
-	# sort merged file
-	samtools sort -n $bam.merged.chr5.bam $bam.merged.chr5.sorted
-	samtools index $bam.merged.chr5.sorted.bam
-	bedtools bamtofastq -i $bam.merged.chr5.sorted.bam -fq	$bam.chr5.R1.fq -fq2	$bam.chr5.R2.fq
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         f)
+             FILE=$OPTARG
+             ;;
+         d)
+             DIR=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
 done
+
+## Handle empty arguments
+
+if [[ -z $FILE ]]; then usage; exit 1; fi
+if [[ -z $DIR ]]; then usage; exit 1; fi
+
+cd $DIR
+
+INPUT=$FILE
+OUTDIR=$DIR/01_chr5fastq
+
+mkdir -p $OUTDIR
+
+echo "Starting Chr5 Extraction "`date`
+
+cat $INPUT | while read bam
+do
+  	echo "Starting extraction of $bam on "`date`
+        prefix=$(basename $bam .bam)
+        
+	echo "Getting all mapped reads where both mates are mapped..."
+	samtools view -bh -f 1 -F 12 $bam chr5 > $OUTDIR/$bam.paired.chr5.bam
+	
+	echo "Getting reads where R1 is unmapped, R2 is mapped in a primary alignment..."
+	samtools view -bh -f 4 -F 264 $bam chr5 > $OUTDIR/$bam.matemapped.chr5.bam
+ 
+	echo "Getting reads where R1 is mapped, and R2 is unmapped..."
+	samtools view -bh -f 8 -F 260 $bam chr5 > $OUTDIR/$bam.mateunmapped.chr5.bam
+
+	echo "Merge filtered bam files for $bam..."
+	samtools merge $OUTDIR/$bam.merged.chr5.bam  $OUTDIR/$bam.paired.chr5.bam $OUTDIR/$bam.matemapped.chr5.bam $OUTDIR/$bam.mateunmapped.chr5.bam
+
+	echo "Sort merged file..."
+	samtools sort -n $OUTDIR/$bam.merged.chr5.bam $OUTDIR/$bam.merged.chr5.sorted
+	samtools index $OUTDIR/$bam.merged.chr5.sorted.bam
+	bedtools bamtofastq -i $OUTDIR/$bam.merged.chr5.sorted.bam -fq	$OUTDIR/$bam.chr5.R1.fq -fq2	$OUTDIR/$bam.chr5.R2.fq
+	
+	echo "Deleting temporary files..."
+	rm $OUTDIR/$bam.paired.chr5.bam
+	rm $OUTDIR/$bam.matemapped.chr5.bam
+	rm $OUTDIR/$bam.mateunmapped.chr5.bam
+        
+        echo "Finished extraction of $bam on "`date`
+
+done
+
+echo "Completed Chr5 Extraction on "`date`
 
 
